0babfac83eb72163b2e4fcbfcd6f66b0196da026
Alexander <al.tulin@gmail.com>
2022-06-24 11:39:32 +0800

    test

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 const {all} = require('core-js/fn/promise');
 
 module.exports = {
-	singleQuote: true,
-	bracketSpacing: false,
+	// singleQuote: true,
+	// bracketSpacing: false,
 	trailingComma: all,
-	semi: true,
+	// semi: true,
 };
